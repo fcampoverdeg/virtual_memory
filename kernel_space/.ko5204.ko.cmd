817f9b4d3b7bb55830364632b9256ec01961cd62
houndsito <fcampoverdeg@gmail.com>
2025-12-01 10:35:52 -0500

    Clean kernel_space directory: remove build artifacts and add .gitignore

--- kernel_space/.ko5204.ko.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_ko5204.ko := ld -r -m elf_x86_64 -z noexecstack --no-warn-rwx-segments --build-id=sha1  -T /usr/src/linux-headers-6.14.0-33-generic/scripts/module.lds -o ko5204.ko ko5204.o ko5204.mod.o .module-common.o
--- kernel_space/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_modules.order := {   echo ko5204.o; :; } > modules.order
